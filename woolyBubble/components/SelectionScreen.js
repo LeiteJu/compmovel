ed949bc3b55214c4ef58a750b6340cf73962ed87
Lucy Anne de Omena Evangelista <lucy.omena@live.com>
2021-10-10 03:49:08 -0300

    Add scketch of second screen

--- woolyBubble/components/SelectionScreen.js
+++ woolyBubble/components/SelectionScreen.js
@@ -8,9 +8,33 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 export default function SelectionScreen({navigation}) {
     return (
         <View style={styles.SelectionScreen}>
-		<Text>Details Screen</Text>
-        <Button title="Go to hotel" onPress={() => navigation.navigate('Hotel') } />
-        <Button title="Go back button" onPress={() => navigation.goBack() } />
+            
+        <Text> Olá, pessoa! </Text>
+		<Text style={{flex: 1}}>Hotéis mais amados:</Text>
+
+        <View style={styles.SelectionScreenColumnHotel}>
+            <View style={styles.SelectionScreenHotel}>
+                <Text> Adicionar imagem e link para hotel 1 </Text>
+                <View style={styles.SelectionScreenHotelRow}>
+                    <Text> Endereco do hotel 1 </Text>
+                    <Button title="Go to hotel" onPress={() => navigation.navigate('Hotel') } />
+                </View>
+            </View>
+            <View style={styles.SelectionScreenHotel}>
+                <Text> Adicionar imagem e link para hotel 2 </Text>
+                <View style={styles.SelectionScreenHotelRow}>
+                    <Text> Endereco do hotel 2 </Text>
+                    <Button title="Go to hotel" onPress={() => navigation.navigate('Hotel') } />
+                </View>
+            </View>
+            <View style={styles.SelectionScreenHotel}>
+                <Text> Adicionar imagem e link para hotel 3 </Text>
+                <View style={styles.SelectionScreenHotelRow}>
+                    <Text> Endereco do hotel 3 </Text>
+                    <Button title="Go back button" onPress={() => navigation.goBack() } />
+                </View>
+            </View>
+        </View>
 	  </View>
     )
 }
@@ -22,6 +46,32 @@ const styles = StyleSheet.create({
       backgroundColor: '#fcdcdb',
       alignItems: 'center',
       justifyContent: 'center',
+      width: '100%'
+      
+    },
+    SelectionScreenColumnHotel: {
+        flex: 4,
+        alignItems: 'stretch',
+        width: '100%',
+        justifyContent: 'space-around',
+        backgroundColor: '#FAFAFA', 
+        
+        
+        
+    },
+    SelectionScreenHotel: {
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: "#6E59FF",
+        padding: 10,
+        width: '100%'
+        
+    },
+    SelectionScreenHotelRow: {
+        flexDirection: 'row' //default is column
+        , alignItems: 'center',
+        justifyContent: 'flex-end',
+        
     },
   
 })
